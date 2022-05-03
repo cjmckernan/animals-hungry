c416372c2e521f685a962ad3fb5cd1bfeeeea10d
cjmckernan <conorjmckernan@gmail.com>
2022-05-03 15:40:44 +0100

    added flag to check meals fed; removed meal number

--- pages/index.js
+++ pages/index.js
@@ -17,8 +17,7 @@ const Home = ({ animals }) => {
               return (
                 <tr className="py-6" key={animal._id}>
                   <td><div className="text-2xl text-center">{animal.name}</div></td>
-                  <td className='inline-flex px-6'>{Home.generateRadioButtons(animal.numberOfMeals)}</td>
-                  <td>{animals.currentMeal}</td>
+                  <td className='inline-flex px-6'>{Home.generateRadioButtons(animal.numberOfMeals, animal.currentMeal)}</td>
                 </tr>
               )
             })}
@@ -29,18 +28,23 @@ const Home = ({ animals }) => {
   )
 }
 
-Home.generateRadioButtons = (numMeals) => {
+Home.generateRadioButtons = (numMeals, currentMeal) => {
   let radioButtons = [];
+  let mealFed
   for (let index = 1; index < numMeals+1; index++) {
+    mealFed = currentMeal >= index ? true : false
+
     radioButtons.push(
       <div key={index} className="form-check px-6">
-        <input className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 my-1 align-top bg-no-repeat bg-center bg-contain float-left cursor-pointer" type="checkbox" />
-        <label className="form-check-label inline-block text-white-800 px-2" >Meal {index}</label>
+        <input defaultChecked={mealFed} className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 my-1 align-top bg-no-repeat bg-center bg-contain float-left cursor-pointer" type="checkbox" />
       </div> 
     )
   }
   return radioButtons;
 }
+
+
+
 
 Home.getInitialProps = async () => {
   const res = await fetch('http://localhost:3000/api/animals')
