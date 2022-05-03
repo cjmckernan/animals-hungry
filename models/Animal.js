6685cbc7352a8b6db2e33d0972fb50a0c7a6ecea
cjmckernan <conorjmckernan@gmail.com>
2022-05-03 01:35:25 +0100

    added mongoose and animals schema

---
+++ models/Animal.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+const AnimalSchema = new mongoose.Schema({
+    name: {
+        type: String, 
+        required: [true, 'Please add a name'],
+        unique: true,
+        trim: true,
+        maxlength: [40, 'Name cannot be more than 40 characters']
+    },
+    numberOfMeals: {
+        type: Number,
+        required: true
+    }
+})
+
+module.exports = mongoose.models.Animal || mongoose.model('Animal', AnimalSchema)
